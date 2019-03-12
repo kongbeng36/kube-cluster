f58396a889fd27181880cd03076b247dbe0fa443
Imran Iheng <imran.ih@inet.co.th>
2019-03-12 11:29:18 +0700

    message

--- 1-install-docker.sh
+++ 1-install-docker.sh
@@ -3,4 +3,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo systemctl status docker
+echo 'install docker SUCESS !!!'
+sudo usermod -aG docker ${USER}
+su - ${USER}
+id -nG
+echo 'Executing the Docker Command Without Sudo ';
 
