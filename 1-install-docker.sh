e1babc1d5bb117376b789ce9823baba5fb0ff131
kongbeng36 <48008362+kongbeng36@users.noreply.github.com>
2019-08-27 16:23:28 +0700

    Update 1-install-docker.sh

--- 1-install-docker.sh
+++ 1-install-docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
